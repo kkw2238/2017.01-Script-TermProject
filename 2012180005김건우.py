7e22683d0ab9ecdf1c791d8702bb4966f6154657
kkw2238 <kkw2238@naver.com>
2017-05-23 23:53:55 +0900

    2017.05.23
    함수 수정

--- 2012180005김건우.py
+++ 2012180005김건우.py
@@ -1,12 +1,83 @@
-import http.client
-conn = http.client.HTTPConnection("apis.data.go.kr")
-conn.request("GET",
-             """1430000/StdPatStsInfoService/getRealtimeStatisticsInfo?serviceKey=
-             RfXMKfQ7poleOdmbgzrOPTeccpDIkYlwmQCxZNI3mNo3OBYPX22pBUNLBx%2F%2BB7KLw21aM43w5YSxW9yAYnuMfQ%3D%3D
-             &numOfRows=10&pageSize=10&pageNo=1&startPage=1&resultType=xml&workverymn=201612&stdorgancd=ETSI&
-             countryclass=%EA%B8%B0%EC%97%85&dtltech1=3GPP&dtltech2=LTE&declarer_adjust=Apple&
-             declarercountry_adjust=%EB%AF%B8%EA%B5%AD&club2050yn=N&oecdyn=N&comptype=%EB%AF%B8%EA%B5%AD""")
-req = conn.getresponse()
-print(req.status,req.reason)
-cLen = req.getgeader("Content-Length")
-req.read(cLen)
+# -*- coding: cp949 -*-
+
+from http.client import HTTPConnection
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib
+import sys
+
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
+
+##http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?
+
+##global
+conn = None
+# regKey = 'z2mxgo5Uk0sWx%2Fchtf0SL2R3i4RnnH4VvlkTtvUwC1ZJYUNKCBu3keO0oBQz9yhg8Tu8q7xpK1JIJ2naACLMiA%3D%3D'
+
+# 네이버 OpenAPI 접속 정보 information
+server = "openapi.airkorea.or.kr"
+
+def userURIBuilder(server, **user):
+    str = "http://" + server + "/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty" + "?"
+    for key in user.keys():
+        str += key + "=" + user[key] + "&"
+    return str
+
+
+def connectOpenAPIServer():
+    global conn, server
+    conn = HTTPConnection(server)
+
+# stationName=종로구&
+# dataTerm=month&
+# pageNo=1&
+# numOfRows=10&
+# ServiceKey=z2mxgo5Uk0sWx%2Fchtf0SL2R3i4RnnH4VvlkTtvUwC1ZJYUNKCBu3keO0oBQz9yhg8Tu8q7xpK1JIJ2naACLMiA%3D%3D&
+# ver=1.3
+
+def getDataToApi():
+    global server, regKey, conn
+    if conn == None:
+        connectOpenAPIServer()
+    uri = userURIBuilder(server, stationName=urllib.parse.quote("종로구"), dataTerm="month", pageNo="1", numOfRows="10",ServiceKey=
+    """z2mxgo5Uk0sWx%2Fchtf0SL2R3i4RnnH4VvlkTtvUwC1ZJYUNKCBu3keO0oBQz9yhg8Tu8q7xpK1JIJ2naACLMiA%3D%3D""",ver="1.3")
+    print(uri)
+    conn.request("GET", uri)
+    req = conn.getresponse()
+    print(req.status)
+    if int(req.status) == 200:
+        print("Data Downloading Complete!")
+        return extractBookData(req.read())
+    else:
+        print("OpenAPI request has been failed!! please retry")
+        return None
+
+
+def extractBookData(strXml):
+    from xml.etree import ElementTree
+    tree = ElementTree.fromstring(strXml)
+    print(strXml)
+    # Book 엘리먼트를 가져옵니다.
+    itemElements = tree.getiterator("item")  # return list type
+    print(itemElements)
+    for item in itemElements:
+        isbn = item.find("isbn")
+        strTitle = item.find("title")
+        print(strTitle)
+        if len(strTitle.text) > 0:
+            return {"ISBN": isbn.text, "title": strTitle.text}
+
+
+def checkConnection():
+    global conn
+    if conn == None:
+        print("Error : connection is fail")
+        return False
+    return True
+
+def main() :
+
+    getDataToApi()
+
+
+main()
