07c60b947edb1945e77891a8f388921bc676cbfe
kkw2238 <kkw2238@naver.com>
2017-06-06 23:51:42 +0900

    2017.06.06 화
    TKinter 구상

---
+++ OutputTKinter.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import font
+import tkinter.messagebox
+g_Tk = Tk()
+g_Tk.geometry("400x600+750+200")
+DataList = []
+
+
+def SearchLibrary():
+    import http.client
+    from xml.dom.minidom import parse, parseString
+
+def InitRenderText():
+    global RenderText
+
+def SearchButtonAction():
+    global SearchListBox
+
+def InitInputLabel():
+    global InputLabel
+
+def InitSearchButton():
+    pass
+
+def InitSearchListBox():
+    global SearchListBox
+
+def InitTopText():
+    pass
+
+InitTopText()
+InitSearchListBox()
+InitInputLabel()
+InitSearchButton()
+InitRenderText()
+#InitSendEmailButton()
+#InitSortListBox()
+#InitSortButton()
+
+g_Tk.mainloop()
